6e644209a540ea6a766c7d33036994e45efef882
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-12 18:26:42 -0300

    feat: state manages input

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
 /** jsx @createElement */
-import { createElement } from "./Framework.js";
+import { useState, createElement } from "./Framework.js";
 
 export default function TodoApp() {
+  const [newTodo, setNewTodo] = useState("");
+
   return (
-    <div>
+    <div className="container">
       <h1>Todo App</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new todo"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
       <ul></ul>
     </div>
   );
