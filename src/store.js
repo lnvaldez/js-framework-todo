62c10ac5656575fdbb4e0e6d848e6bb6a385a076
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-22 12:08:25 -0300

    refactor: app store

--- src/store.js
+++ src/store.js
@@ -1,46 +1,6 @@
-import Framework from "./Framework";
+import Framework from "./Framework.js";
+import { todoReducer } from "./reducers/todoReducer.js";
 
-const store = Framework.createStore((state = { todos: [] }, action) => {
-  switch (action.type) {
-    case "ADD_TODO":
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          { text: action.payload, completed: false, editing: false },
-        ],
-      };
-    case "REMOVE_TODO":
-      return {
-        ...state,
-        todos: state.todos.filter((_, i) => i !== action.payload),
-      };
-    case "TOGGLE_TODO":
-      return {
-        ...state,
-        todos: state.todos.map((todo, i) =>
-          i === action.payload ? { ...todo, completed: !todo.completed } : todo
-        ),
-      };
-    case "START_EDIT":
-      return {
-        ...state,
-        todos: state.todos.map((todo, i) =>
-          i === action.payload ? { ...todo, editing: true } : todo
-        ),
-      };
-    case "SAVE_EDIT":
-      return {
-        ...state,
-        todos: state.todos.map((todo, i) =>
-          i === action.payload.index
-            ? { ...todo, text: action.payload.text, editing: false }
-            : todo
-        ),
-      };
-    default:
-      return state;
-  }
-});
+const store = Framework.createStore(todoReducer);
 
 export default store;
